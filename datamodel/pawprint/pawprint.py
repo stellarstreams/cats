8368ed597328de33a08158487e576fcad07caa30
Robyn Sanderson <robynes@sas.upenn.edu>
2022-11-03 09:52:56 -0700

    added methods to add footprints to pawprint for cmd and pm

--- datamodel/pawprint/pawprint.py
+++ datamodel/pawprint/pawprint.py
@@ -5,24 +5,28 @@ import asdf
 import astropy.units as u
 from astropy.coordinates import SkyCoord
 from gala.coordinates import GreatCircleICRSFrame
+import numpy as np
 
 
 #class densityClass: #TODO: how to represent densities?
 
 class Footprint2D(dict):
-    def __init__(self, vertex_coordinates, footprint_type):
-        if footprint_type='sky':
+    def __init__(self, vertex_coordinates, footprint_type, stream_frame=None):
+        if footprint_type=='sky':
             if isinstance(vertex_coordinates, SkyCoord):
-                self.vertices = vertex_coordinates
+                vc = vertex_coordinates
             else:
-                self.vertices = SkyCoord(vertex_coordinates)
+                vc = SkyCoord(vertex_coordinates)
+            self.edges = vc
+            self.vertices = np.array([vc.transform_to(stream_frame).phi1, vc.transform_to(stream_frame).phi2]).T
 
-        elif footprint_type='cartesian':
+        elif footprint_type=='cartesian':
+            self.edges = vertex_coordinates
             self.vertices = vertex_coordinates
 
-        #default is to assume cartesian coords - should it be skycoord?
-        self.poly = mpl_path(vertices)
-
+        self.stream_frame = stream_frame
+        self.footprint = mpl_path(self.vertices)
+        
     @classmethod
     def from_vertices(cls, vertex_coordinates, footprint_type):
         return cls(vertices,footprint_type)
@@ -41,9 +45,26 @@ class Footprint2D(dict):
 
     def get_vertices_from_box(min1, max1, min2, max2):
         return [[min1,min2],[min1,max2],[max1,min2],[max1,max2]]
+    
+    def inside_footprint(self,data):
+        if isinstance(data, SkyCoord):
+            if self.stream_frame is None:
+                print("can't!")
+                return
+            else:
+                pts = np.array([data.transform_to(self.stream_frame).phi1.value,data.transform_to(self.stream_frame).phi2.value]).T
+                return self.footprint.contains_points(pts)
+        else:
+            return self.footprint.contains_points(data)
 
-    def inside_poly(data):
-        return self.poly.contains_points(data)
+    def export(self):
+        data = {}
+        data['stream_frame']=self.stream_frame
+        data['vertices']=self.vertices
+        data['footprint_type']=self.footprint_type
+        return data
+
+
 
 
 
@@ -58,6 +79,7 @@ class pawprintClass(dict):
 
         '''
 
+    
     def __init__(self, data):
         
         #we need a way to specify and load the vertices for the footprints. 
@@ -69,16 +91,16 @@ class pawprintClass(dict):
         self.pawprint_ID = data['pawprint_ID']
         self.stream_frame = data['stream_frame']
         self.width = data['width']
-        self.skyprint = {'stream':Footprint2D(data['stream_vertices'],type='sky'), 
-                        'background':Footprint2D(data.['background_vertices'],type='sky')} 
+        self.skyprint = {'stream':Footprint2D(data['stream_vertices'],footprint_type='sky',stream_frame=self.stream_frame), 
+                        'background':Footprint2D(data['background_vertices'],footprint_type='sky',stream_frame=self.stream_frame)} 
         #WG3: how to implement distance dependence in isochrone selections?
         self.cmd_filters = data['cmd_filters']
         self.cmdprint = {}
         if self.cmd_filters is not None:
             for k in data.cmd_filters.keys():
-                self.cmdprint[k] = Footprint2D(data['cmd_vertices'][k], type='cartesian')
+                self.cmdprint[k] = Footprint2D(data['cmd_vertices'][k], footprint_type='cartesian')
         if data['pm_vertices'] is not None:
-            self.pmprint = Footprint2D(data['pm_vertices'],type='sky') #polygon(s) in proper-motion space mu_phi1, mu_phi2
+            self.pmprint = Footprint2D(data['pm_vertices'],footprint_type='sky') #polygon(s) in proper-motion space mu_phi1, mu_phi2
         
         self.track = data['track']
 
@@ -87,24 +109,52 @@ class pawprintClass(dict):
         #TODO: load all the stuff from the file into a dictionary called data and call the main init function
         ...
         return cls(data)
-
-
         
 
     
     @classmethod
     def pawprint_from_galstreams(cls,stream_name,pawprint_ID):
+        
+        
+        galstreams_tracks = '../../galstreams/galstreams/tracks/'
+    
+        def _make_track_file_name(stream_name,pawprint_ID):
+            return galstreams_tracks+'track.st.'+stream_name+'.'+pawprint_ID+".ecsv"
+    
+        def _make_summary_file_name(stream_name,pawprint_ID):
+            return galstreams_tracks+'track.st.'+stream_name+'.'+pawprint_ID+".summary.ecsv"
+        
+        def _get_stream_frame_from_file(summary_file):
+            t = apt.QTable.read(summary_file)
+        
+            x = dict()
+            atts = [x.replace('mid.','') for x in t.keys() if 'mid' in x ]
+            for att in atts:  #we're effectively looping over skycoords defined for mid here (ra, dec, ...)
+                x[att] = t[f'mid.{att}'][0]   #<- make sure to set it up as a scalar. if not, frame conversions get into trouble
+            mid_point = SkyCoord(**x) 
+
+            x = dict()
+            atts = [x.replace('pole.','') for x in t.keys() if 'pole' in x ]
+            for att in atts:  #we're effectively looping over skycoords defined for pole here (ra, dec, ...)
+                x[att] = t[f'pole.{att}'][0]
+            #Make sure to set the pole's distance attribute to 1 (zero causes problems, when transforming to stream frame coords)
+            x["distance"] = 1.*u.kpc   #it shouldn't matter, but if it's zero it does crazy things
+            mid_pole = SkyCoord(**x)
+
+            return GreatCircleICRSFrame(pole=mid_pole, ra0=mid_point.icrs.ra)
+
         data = {}
         data['stream_name'] = stream_name
         data['pawprint_ID'] = pawprint_ID
 
         track_file = _make_track_file_name(stream_name,pawprint_ID) 
         summary_file = _make_summary_file_name(stream_name,pawprint_ID) 
-        data['stream_frame'] = _get_stream_frame_from_galstreams_summary_file(summary_file)
-        data['track'] = gst.Track6D(data['stream_name'], track_file, summary_file)
+        data['stream_frame'] = _get_stream_frame_from_file(summary_file)
+
+        data['track'] = gst.Track6D(stream_name=data['stream_name'], track_name=data['pawprint_ID'], track_file=track_file, summary_file=summary_file)
         data['width'] = 1.0*u.deg
-        data['stream_vertices'] = gst.create_sky_polygon_footprint_from_track(data['track'],frame=None, width=data['width'], phi2_offset=0.*u.deg)
-        data['background_vertices'] = gst.create_sky_polygon_footprint_from_track(data['track'],frame=None, width=data['width'], phi2_offset=3.*u.deg)
+        data['stream_vertices'] = data['track'].create_sky_polygon_footprint_from_track(width=data['width'], phi2_offset=0.*u.deg)
+        data['background_vertices'] = data['track'].create_sky_polygon_footprint_from_track(width=data['width'], phi2_offset=3.*u.deg)
         data['cmd_filters'] = None
         data['cmd_vertices'] = None
         data['pm_vertices'] = None
@@ -113,6 +163,19 @@ class pawprintClass(dict):
         return cls(data)
 
 
+    def add_cmd_footprint(self, new_footprint, color, mag, name):
+        if self.cmd_filters is None:
+            self.cmd_filters = {name:[color, mag]}
+            self.cmdprint = {name: new_footprint}
+        else:
+            self.cmd_filters[name] = [color,mag]
+            self.cmdprint[name] = new_footprint
+
+    def add_pm_footprint(self, new_footprint, name):
+        if self.pmprint is None:
+            self.pmprint = {name: new_footprint}
+        else:
+            self.pmprint[name] = new_footprint
 
 
     def save_pawprint(self):
@@ -124,38 +187,15 @@ class pawprintClass(dict):
             'cmd_filters': cmd_filters,
             'width':width,
             'on_stream':{
-                        'sky':skyprint['stream'],
-                        'cmd':cmdprint,
-                        'pm':pmprint
+                        'sky':skyprint['stream'].export(),
+                        'cmd':cmdprint.export(),
+                        'pm':pmprint.export()
                         },
-            'off_stream':skyprint['background'],
+            'off_stream':skyprint['background'].export(),
             'track':track
         }
         out = asdf.AsdfFile(tree)
         out.write_to(fname)
+    
 
-    def _make_track_file_name(stream_name,pawprint_ID):
-        return 'track.st'+stream_name+pawprint_ID+".ecsv"
-    
-    def _make_summary_file_name:
-        return 'track.st'+stream_name+pawprint_ID+".summary.ecsv"
-        
-    def _get_stream_frame_from_galstreams_summary_file(summary_file):
-        t = apt.Table.read(summary_file)
-        
-        x = dict()
-        atts = [x.replace('mid.','') for x in t.keys() if 'mid' in x ]
-        for att in atts:  #we're effectively looping over skycoords defined for mid here (ra, dec, ...)
-            x[att] = t[f'mid.{att}'][0]   #<- make sure to set it up as a scalar. if not, frame conversions get into trouble
-        mid_point = ac.SkyCoord(**x) 
-    
-        x = dict()
-        atts = [x.replace('pole.','') for x in t.keys() if 'pole' in x ]
-        for att in atts:  #we're effectively looping over skycoords defined for pole here (ra, dec, ...)
-            x[att] = t[f'pole.{att}'][0]
-        #Make sure to set the pole's distance attribute to 1 (zero causes problems, when transforming to stream frame coords)
-        x["distance"] = 1.*u.kpc   #it shouldn't matter, but if it's zero it does crazy things
-        mid_pole = SkyCoord(**x)
-        
-        return GreatCircleICRSFrame(pole=mid_pole, ra0=mid_point.icrs.ra)
 
