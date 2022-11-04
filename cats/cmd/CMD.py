7a900db784076819797f4fe75a1ccca869c1dc93
Jack Warfield <jtw5zc@virginia.edu>
2022-11-04 16:18:42 -0700

    bg cmd, error function for cmd width

--- cats/cmd/CMD.py
+++ cats/cmd/CMD.py
@@ -12,6 +12,7 @@ import scipy
 from scipy.interpolate import interp1d
 from scipy.signal import correlate2d
 from ugali.analysis.isochrone import factory as isochrone_factory
+from astropy.convolution import Gaussian1DKernel, convolve
 
 plt.rc(
     "xtick",
@@ -32,10 +33,14 @@ plt.rc(
 
 
 class Isochrone:
-    def __init__(self, cat, age, feh, distance, alpha, sky_poly, pm_poly):
+    def __init__(
+        self, cat, age, feh, distance, alpha, sky_poly, pm_poly, back_sky_poly
+    ):
 
         """
         Defining variables loaded into class.
+        Note- the assumption here is that the on-stream and off-stream sky
+              polygon are the same area
 
         ------------------------------------------------------------------
 
@@ -47,6 +52,7 @@ class Isochrone:
         alpha = alpha/Fe
         sky_poly = Polygon mask from sky selection in (RA, Dec).
         pm_poly = Polygon mask from proper motions in (pm1, pm2).
+        back_sky_poly = Polygon mask for background
         """
         # Maybe include distance track?
         # Pull survey from catalog?
@@ -59,12 +65,15 @@ class Isochrone:
         self.pm_poly = pm_poly
         self.x_shift = 0
         self.y_shift = 0
+        self.back_sky_poly = back_sky_poly
         self.survey = "ps1"
+        self.cmd_mask = 0
+        self.cmd_poly = 0
 
     def sel_sky(self):
 
         """
-        Initialising the on-sky polygon mask to return only contained sources.
+        Initialising the on-sky and off-sky polygon masks to return only contained/bkg sources.
         """
         cat = self.cat
         on_poly_patch = mpl.patches.Polygon(
@@ -74,6 +83,14 @@ class Isochrone:
         on_mask = on_poly_patch.get_path().contains_points(on_points)
 
         self.on_skymask = on_mask
+
+        off_poly_patch = mpl.patches.Polygon(
+            self.back_sky_poly, facecolor="none", edgecolor="k", linewidth=2
+        )
+        off_points = np.vstack((cat["phi1"], cat["phi2"])).T
+        off_mask = off_poly_patch.get_path().contains_points(off_points)
+
+        self.off_skymask = off_mask
 
     def sel_pm(self):
 
@@ -169,35 +186,60 @@ class Isochrone:
 
         self.x_edges = xedges
         self.y_edges = yedges
+        self.x_bins = x_bins
+        self.y_bins = y_bins
         self.CMD_data = data.T
 
-    def make_poly(self, iso_low, iso_high, maxmag=26, minmag=14):
-        """
-        Generate the CMD polygon mask.
+        background_data, _, _ = np.histogram2d(
+            (tab["g0"] - tab["r0"])[self.on_pmmask & self.off_skymask],
+            tab["g0"][self.on_pmmask & self.off_skymask],
+            bins=[x_bins, y_bins],
+            density=True,
+        )
 
-        ------------------------------------------------------------------
+        self.background_CMD_data = background_data.T
+
+        # Is this used anywhere currently? Can add as a plot further down... this should also be divided by the relative area
+        self.subtracted_CMD_data = (self.CMD_data) - (self.background_CMD_data)
+
+    def make_poly(self, iso_model, tolerance, maxmag=26, minmag=14):
+        """
+        Generate the CMD polygon that depends on the errors in the photometric data
+        In other words, the width of the selection should increase for fainter g-magnitude.
+
+        --------------------------------------------------------------------
 
         Parameters:
-        iso_low: spline function describing the "left" bound of the theorietical isochrone
-        iso_high: spline function describing the "right" bound of the theoretical isochrone
+        iso_model: spline function describing best fitting theoretical isochrone model
+        phot_errors: an 2D array or a spline that describes the size of the photometric errors as a function of g-magnitude
         maxmag: faint limit of theoretical isochrone, should be deeper than all data
         minmag: bright limit of theoretical isochrone, either include just MS and subgiant branch or whole isochrone
 
         Returns:
         cmd_poly: Polygon vertices in CMD space.
         cmd_mask: Boolean mask in CMD sapce.
-
         """
 
-        mag1, mag2 = "g0", "r0"
-        mag_vals = np.arange(minmag, maxmag, 0.01)
-        col_low_vals = iso_low(mag_vals)
-        col_high_vals = iso_high(mag_vals)
+        magband1, magband2 = "g0", "r0"
+        # mag_vals = np.arange(minmag, maxmag, 0.01)
 
+        ##Use the photometric error to determine the width of the isochrone (tolerance)
+        self.errFn_default()  # to get self.gerrs
+        gerrs = np.array(self.gerrs(self.mag1))
+
+        tol = np.sqrt(
+            2 * gerrs**2 + tolerance**2
+        )  # 1-sigma tolerance on the error added in quadrature
+        self.tol = tol
+
+        col_low_vals = iso_model(self.mag1) - self.tol
+        col_high_vals = iso_model(self.mag1) + self.tol
+
+        # from here it is the same as make_poly
         cmd_poly = np.concatenate(
             [
-                np.array([col_low_vals, mag_vals]).T,
-                np.flip(np.array([col_high_vals, mag_vals]).T, axis=0),
+                np.array([col_low_vals, self.mag1]).T,
+                np.flip(np.array([col_high_vals, self.mag1]).T, axis=0),
             ]
         )
 
@@ -205,13 +247,14 @@ class Isochrone:
         cmd_poly_patch = mpl.patches.Polygon(
             cmd_poly, facecolor="none", edgecolor="k", linewidth=2
         )
-        cmd_points = np.vstack((self.cat[mag1] - self.cat[mag2], self.cat[mag1])).T
+        cmd_points = np.vstack(
+            (self.cat[magband1] - self.cat[magband2], self.cat[magband1])
+        ).T
         cmd_mask = cmd_poly_patch.get_path().contains_points(cmd_points)
 
         return cmd_poly, cmd_mask
 
     def correct_isochrone(self):
-
         """
         Correlate the 2D histograms from the data and the
         theoretical isochrone to find the shift in color
@@ -236,6 +279,7 @@ class Isochrone:
         y, x = np.unravel_index(np.argmax(ccor2d), ccor2d.shape)
         self.x_shift = (x - len(ccor2d[0]) / 2.0) * (self.x_edges[1] - self.x_edges[0])
         self.y_shift = (y - len(ccor2d) / 2.0) * (self.y_edges[1] - self.y_edges[0])
+        self.signal = signal
 
     def simpleSln(
         self,
@@ -281,7 +325,15 @@ class Isochrone:
             mag1 + magoff, mag1 - mag2 + coloff, fill_value="extrapolate"
         )
 
-        cmd_poly, cmd_mask = self.make_poly(iso_low, iso_high, maxmag=24, minmag=14)
+        cmd_poly, cmd_mask = self.make_poly(
+            iso_model,
+            tolerance,
+            maxmag=24,
+            minmag=14,
+        )
+
+        self.cmd_poly = cmd_poly
+        self.cmd_mask = cmd_mask
 
         return cmd_poly, cmd_mask, iso_model, iso_low, iso_high
 
@@ -348,7 +400,7 @@ class Isochrone:
 
         return fig
 
-    def convolve_1d(probabilities, mag_err):
+    def convolve_1d(self, line, mag_err):
 
         """
         1D Gaussian convolution.
@@ -360,16 +412,14 @@ class Isochrone:
         mag_err: Uncertainty in the magnitudes.
 
         """
-        self.probabilities = probabilities
-        self.mag_err = mag_err
 
-        sigma = mag_err / self.ybin  # error in pixel units
+        sigma = mag_err / (self.y_bins[1] - self.y_bins[0])  # error in pixel units
         kernel = Gaussian1DKernel(sigma)
-        convolved = convolve(probabilities, kernel)
+        convolved = convolve(line, kernel)
 
-        convolved.self = convolved
+        return convolved
 
-    def convolve_errors(self, g_errors, r_errors, intr_err=0.1):
+    def convolve_errors(self, intr_err=0.1):
 
         """
 
@@ -378,31 +428,43 @@ class Isochrone:
         ------------------------------------------------------------------
 
         Parameters:
-        g_errors: g magnitude uncertainties.
-        r_errors: r magnitude uncertainties.
         intr_err: Free to set. Default is 0.1.
 
         """
 
-        for i in range(len(probabilities)):
-            probabilities[i] = convolve_1d(
-                probabilities[i],
+        self.smoothed_signal = np.zeros((len(self.signal), len(self.signal[0])))
+        for i in range(len(self.smoothed_signal)):
+            self.smoothed_signal[i] = self.convolve_1d(
+                self.signal[i],
                 np.sqrt(
-                    g_errors(self.x_bins[i]) ** 2
-                    + r_errors(self.y_bins[i]) ** 2
+                    self.gerrs(self.x_bins[i]) ** 2
+                    + self.rerrs(self.y_bins[i]) ** 2
                     + intr_err**2
                 ),
-                sel.fx_bins[1] - self.x_bins[0],
             )
 
-        self.probabilities = probabilities
-
-    def errFn(self):
+    def errFn_default(self):
 
         """
-        Generate the errors for the magnitudes?
+        Generate the errors given input magnitude, based on the input survey
         """
 
+        if self.survey == "ps1":
+            self.gerrs = lambda x: 0.00363355415 + np.exp((x - 23.9127145) / 1.09685211)
+            self.rerrs = lambda x: 0.00363355415 + np.exp((x - 23.9127145) / 1.09685211)
+            print("using panstarrs")
+        elif self.survey == "des":
+            self.gerrs = lambda x: 0.0010908679647672335 + np.exp(
+                (x - 27.091072029215375) / 1.0904624484538419
+            )
+            self.rerrs = lambda x: 0.0010908679647672335 + np.exp(
+                (x - 27.091072029215375) / 1.0904624484538419
+            )
+        else:
+            self.gerrs = lambda x: 0.0
+            self.rerrs = lambda x: 0.0
+
+    def errFn_data(self):
         gerrs = np.zeros(len(self.y_bins))
         rerrs = np.zeros(len(self.x_bins))
 
@@ -419,3 +481,89 @@ class Isochrone:
 
         self.gerrs = gerrs
         self.rerrs = rerrs
+
+    def hb_mask(self, width=0.5):
+        """
+        Returns a polygon selection along the BHB.
+        Takes a BHB branch from E. Vasiliev and S. Koposov in BP/RP and
+        transforms to the photometric system
+
+        Parameters
+        ----------
+        width : 'float'
+            Required width of the horizontal branch filter
+
+        Returns:
+        -------
+        polygon : 'matplotlib.patches.Polygon'
+            Polygon for PatchCollection (e.g.)
+        hb_2d_array : 'array_like, shape (N, 2)'
+           Horizontal branch widen by added width
+        original_track : 'array_like, shape (N, 2)'
+            Horizontal branch path from E. Vasiliev
+            originaly from S. Koposov.
+
+        """
+
+        mag1, mag2 = "g0", "r0"
+        bp_rp = [
+            -0.15,
+            -0.075,
+            0.0,
+            0.075,
+            0.15,
+            0.225,
+            0.3,
+            0.375,
+            0.45,
+            0.525,
+            0.6,
+            0.675,
+            0.75,
+            0.825,
+            0.9,
+        ]
+
+        abs_g_mag = np.array(
+            [
+                1.56922028,
+                1.30633187,
+                0.98436866,
+                0.8272674,
+                0.73556216,
+                0.66450369,
+                0.64182914,
+                0.5948267,
+                0.62143181,
+                0.61441487,
+                0.62462876,
+                0.60931419,
+                0.60433558,
+                0.57833912,
+                0.56579838,
+            ]
+        )
+
+        original_track = np.vstack([bp_rp, abs_g_mag]).T
+
+        abs_g_mag_bright, abs_g_mag_dimmer = (abs_g_mag - width).tolist(), (
+            abs_g_mag + width
+        ).tolist()
+
+        bp_rp_poly = np.array(bp_rp + bp_rp[::-1])
+        abs_g_mag_poly = (
+            np.array(abs_g_mag_bright + abs_g_mag_dimmer[::-1])
+            + 5 * np.log10(self.distance * 1000)
+            - 5
+        )
+
+        hb_2d_array = np.vstack([bp_rp_poly, abs_g_mag_poly]).T
+
+        hb_polygon = mpl.patches.Polygon(
+            hb_2d_array, facecolor="none", edgecolor="k", linewidth=2
+        )
+
+        hb_points = np.vstack((self.cat[mag1] - self.cat[mag2], self.cat[mag1])).T
+        hb_mask = hb_polygon.get_path().contains_points(hb_points)
+
+        return hb_polygon, hb_mask
